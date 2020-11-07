55731bd5861b883807ebe182161b4025a9ddc91d
rubens-lavor <haryson07@gmail.com>
2020-11-06 23:09:49 -0300

    update

--- nba/nba.c
+++ nba/nba.c
@@ -57,24 +57,367 @@ jogador_t *le_jogadores(char *nome, int *njogadores) {
     return armazena;
 }
 
-int statj_mais_arremessos(jogador_t *jogadores, int njogadores, char tipo){}
+int statj_mais_arremessos(jogador_t *jogadores, int njogadores, char tipo) {
+    int statj_mais_arremessos = 0;
+    int indice = 0;
+    int i = 0;
 
-int statj_mais_cestas(jogador_t *jogadores, int njogadores, char tipo){}
+    if (jogadores == NULL) {
+        return -1;
+    }
 
-int statj_melhor_percentual(jogador_t *jogadores, int njogadores, char tipo){}
+    switch (tipo) {
+        case '2':
+            statj_mais_arremessos = jogadores[0].a2.arremessos;
 
-int statj_jogos(jogador_t *jogadores, int njogadores, char tipo){}
+            for (i = 1; i < njogadores; i++) {
+                if (statj_mais_arremessos < jogadores[i].a2.arremessos) {
+                    statj_mais_arremessos = jogadores[i].a2.arremessos;
+                    indice = i;
+                }
+            }
 
-int statj_idade(jogador_t *jogadores, int njogadores, char tipo){}
+            break;
 
-int statj_minutos(jogador_t *jogadores, int njogadores, char tipo){}
+        case '3':
+            statj_mais_arremessos = jogadores[0].a3.arremessos;
 
-int statj_pontos(jogador_t *jogadores, int njogadores, char tipo){}
+            for (i = 1; i < njogadores; i++) {
+                if (statj_mais_arremessos < jogadores[i].a3.arremessos) {
+                    statj_mais_arremessos = jogadores[i].a3.arremessos;
+                    indice = i;
+                }
+            }
+            break;
 
-int statt_soma_pontos(jogador_t jogadores, int njogadores, char time){}
+        case 'T':
+            statj_mais_arremessos = jogadores[0].aT.arremessos;
 
-float statt_media_pontos(jogador_t jogadores, int njogadores, char time){}
+            for (i = 1; i < njogadores; i++) {
+                if (statj_mais_arremessos < jogadores[i].aT.arremessos) {
+                    statj_mais_arremessos = jogadores[i].aT.arremessos;
+                    indice = i;
+                }
+            }
+            break;
 
-float statt_media_idade(jogador_t jogadores, int njogadores, char time) {}
+        case 'L':
+            statj_mais_arremessos = jogadores[0].aL.arremessos;
 
-int statt_posicao(jogador_t jogadores, int njogadores, char *time, char posicao) {}
+            for (i = 1; i < njogadores; i++) {
+                if (statj_mais_arremessos < jogadores[i].aL.arremessos) {
+                    statj_mais_arremessos = jogadores[i].aL.arremessos;
+                    indice = i;
+                }
+            }
+            break;
+
+        default:
+            return -2;
+            break;
+    }
+
+    return indice;
+}
+
+int statj_mais_cestas(jogador_t *jogadores, int njogadores, char tipo) {
+    int statj_mais_cestas = 0;
+    int indice = 0;
+    int i = 0;
+
+    if (jogadores == NULL) {
+        return -1;
+    }
+
+    switch (tipo) {
+        case '2':
+            statj_mais_cestas = jogadores[0].a2.cestas;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_mais_cestas < jogadores[i].a2.cestas) {
+                    statj_mais_cestas = jogadores[i].a2.cestas;
+                    indice = i;
+                }
+            }
+
+            break;
+
+        case '3':
+            statj_mais_cestas = jogadores[0].a3.cestas;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_mais_cestas < jogadores[i].a3.cestas) {
+                    statj_mais_cestas = jogadores[i].a3.cestas;
+                    indice = i;
+                }
+            }
+            break;
+
+        case 'T':
+            statj_mais_cestas = jogadores[0].aT.cestas;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_mais_cestas < jogadores[i].aT.cestas) {
+                    statj_mais_cestas = jogadores[i].aT.cestas;
+                    indice = i;
+                }
+            }
+            break;
+
+        case 'L':
+            statj_mais_cestas = jogadores[0].aL.cestas;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_mais_cestas < jogadores[i].aL.cestas) {
+                    statj_mais_cestas = jogadores[i].aL.cestas;
+                    indice = i;
+                }
+            }
+            break;
+
+        default:
+            return -2;
+            break;
+    }
+
+    return indice;
+}
+
+int statj_melhor_percentual(jogador_t *jogadores, int njogadores, char tipo) {
+    float statj_melhor_percentual = 0.0;
+    int indice = 0;
+    int i = 0;
+
+    if (jogadores == NULL) {
+        return -1;
+    }
+
+    switch (tipo) {
+        case '2':
+            statj_melhor_percentual = jogadores[0].a2.percentual;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_melhor_percentual < jogadores[i].a2.percentual) {
+                    statj_melhor_percentual = jogadores[i].a2.percentual;
+                    indice = i;
+                }
+            }
+
+            break;
+
+        case '3':
+            statj_melhor_percentual = jogadores[0].a3.percentual;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_melhor_percentual < jogadores[i].a3.percentual) {
+                    statj_melhor_percentual = jogadores[i].a3.percentual;
+                    indice = i;
+                }
+            }
+            break;
+
+        case 'T':
+            statj_melhor_percentual = jogadores[0].aT.percentual;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_melhor_percentual < jogadores[i].aT.percentual) {
+                    statj_melhor_percentual = jogadores[i].aT.percentual;
+                    indice = i;
+                }
+            }
+            break;
+
+        case 'L':
+            statj_melhor_percentual = jogadores[0].aL.percentual;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_melhor_percentual < jogadores[i].aL.percentual) {
+                    statj_melhor_percentual = jogadores[i].aL.percentual;
+                    indice = i;
+                }
+            }
+            break;
+
+        default:
+            return -2;
+            break;
+    }
+
+    return indice;
+}
+
+int statj_jogos(jogador_t *jogadores, int njogadores, char tipo) {
+    int statj_jogos = 0;
+    int indice = 0;
+    int i = 0;
+
+    if (jogadores == NULL) {
+        return -1;
+    }
+
+    switch (tipo) {
+        case '+':
+            statj_jogos = jogadores[0].jogos;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_jogos < jogadores[i].jogos) {
+                    statj_jogos = jogadores[i].jogos;
+                    indice = i;
+                }
+            }
+
+            break;
+
+        case '-':
+            statj_jogos = jogadores[0].jogos;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_jogos > jogadores[i].jogos) {
+                    statj_jogos = jogadores[i].jogos;
+                    indice = i;
+                }
+            }
+            break;
+
+        default:
+            return -2;
+            break;
+    }
+
+    return indice;
+}
+
+int statj_idade(jogador_t *jogadores, int njogadores, char tipo) {
+    int statj_idade = 0;
+    int indice = 0;
+    int i = 0;
+
+    if (jogadores == NULL) {
+        return -1;
+    }
+
+    switch (tipo) {
+        case '+':
+            statj_idade = jogadores[0].idade;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_idade < jogadores[i].idade) {
+                    statj_idade = jogadores[i].idade;
+                    indice = i;
+                }
+            }
+
+            break;
+
+        case '-':
+            statj_idade = jogadores[0].idade;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_idade > jogadores[i].idade) {
+                    statj_idade = jogadores[i].idade;
+                    indice = i;
+                }
+            }
+            break;
+
+        default:
+            return -2;
+            break;
+    }
+
+    return indice;
+}
+
+int statj_minutos(jogador_t *jogadores, int njogadores, char tipo) {
+    int statj_minutos = 0;
+    int indice = 0;
+    int i = 0;
+
+    if (jogadores == NULL) {
+        return -1;
+    }
+
+    switch (tipo) {
+        case '+':
+            statj_minutos = jogadores[0].minutos;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_minutos < jogadores[i].minutos) {
+                    statj_minutos = jogadores[i].minutos;
+                    indice = i;
+                }
+            }
+
+            break;
+
+        case '-':
+            statj_minutos = jogadores[0].minutos;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_minutos > jogadores[i].minutos) {
+                    statj_minutos = jogadores[i].minutos;
+                    indice = i;
+                }
+            }
+            break;
+
+        default:
+            return -2;
+            break;
+    }
+
+    return indice;
+}
+
+int statj_pontos(jogador_t *jogadores, int njogadores, char tipo) {
+    int statj_pontos = 0;
+    int indice = 0;
+    int i = 0;
+
+    if (jogadores == NULL) {
+        return -1;
+    }
+
+    switch (tipo) {
+        case '+':
+            statj_pontos = jogadores[0].pontos;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_pontos < jogadores[i].pontos) {
+                    statj_pontos = jogadores[i].pontos;
+                    indice = i;
+                }
+            }
+
+            break;
+
+        case '-':
+            statj_pontos = jogadores[0].pontos;
+
+            for (i = 1; i < njogadores; i++) {
+                if (statj_pontos > jogadores[i].pontos) {
+                    statj_pontos = jogadores[i].pontos;
+                    indice = i;
+                }
+            }
+            break;
+
+        default:
+            return -2;
+            break;
+    }
+
+    return indice;
+}
+
+int statt_soma_pontos(jogador_t jogadores, int njogadores, char time) {
+}
+
+float statt_media_pontos(jogador_t jogadores, int njogadores, char time) {
+}
+
+float statt_media_idade(jogador_t jogadores, int njogadores, char time) {
+}
+
+int statt_posicao(jogador_t jogadores, int njogadores, char *time, char posicao) {
+}
